51b0cecdbf4d4826c31fa338c6693ef636972691
Shifeng Huang <shifeng.huang@hpe.com>
2016-11-13 14:44:13 +0800

    add practice

---
+++ src/main/java/com/zizaitianyuan/javac2/App.java
@@ -0,0 +1,13 @@
+package com.zizaitianyuan.javac2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

0be7a8f82d1de6a4f278965f8df407154e327c20
jamgao <1742036996@qq.com>
2016-11-13 14:34:56 +0800

    homework

---
+++ String.java
@@ -0,0 +1,75 @@
+package com.zizai.main;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		
+		
+//		for(int i=1;i<=100;i++){
+//			sum=sum+i;
+//		}
+//		int sum=0;
+//		int i=1;
+//		while(1 <= 100){
+//			sum=sum+i;
+//			i++;
+//		}
+//		System.out.println(sum);
+//		int sum=0;
+//		for(int i=1; i<100;i++){
+//			if(i%2 == 0){
+//				continue;
+//			}
+//			sum+=i;
+//		}
+//		System.out.println(sum);
+//		for(int i=1;i<=4;i++){
+//		for(int j = 0;j<i;j++){
+//				System.out.printf("%d*%d=%d",i,j,i*j);
+//			System.out.print("\n");
+//		}
+//		}
+//		int[] array=new int[10];
+//		for(int i:array){
+//			System.out.println(i);
+//		}
+		
+//		int m=100;
+//		int n=m/10;
+//		switch (n) {
+//		case 6:
+//			System.out.println("及格");
+//			break;
+//		case 7:
+//			System.out.println("中等");
+//			break;
+//		case 8:
+//			System.out.println("良好");
+//			break;
+//		case 9:
+//			System.out.println("优秀");
+//			不写break的话就会继续执行下面的程序，这样就可以将90和100
+//		case 10:
+//			System.out.println("满分");
+//			break;
+//		default:
+//			System.out.println("不及格");
+//			break;
+//		}
+		for(int i=0;i<5;i++){
+		 for(int j=0;j<5;j++){
+			System.out.print("*");
+		}System.out.println();
+
+
+	}
+	}
+
+	}
+
+

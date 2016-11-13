1cf9809e1faf14526e7272f89ac962c41e5072d3
lsh <moshou911215@qq.com>
2016-11-13 12:36:07 +0800

    work

---
+++ lsh/DaXing.java
@@ -0,0 +1,24 @@
+package com.zizai.main;
+
+public class DaXing {
+
+ public static void main(String[] args) {
+	// for (int i = 0; i < 4; i++) {
+	// for (int j = 0; j < 4; j++) {
+	// System.out.print("* ");
+	//
+	// }
+	// System.out.println();
+	// }
+	text(6);
+}
+	public static void text(int n) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+
+		}
+	}
+}

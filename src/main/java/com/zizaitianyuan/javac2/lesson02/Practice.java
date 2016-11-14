fc1e12aa53ca1df77ac2239a25f48a311d35c9d2
OhHelloWorld <18255629607@163.com>
2016-11-14 19:48:52 +0800

    jiarui homework

--- src/main/java/com/zizaitianyuan/javac2/lesson02/Practice.java
+++ src/main/java/com/zizaitianyuan/javac2/lesson02/Practice.java
@@ -1,4 +1,4 @@
-package com.zizaitianyuan.javac2.lesson02;
+﻿package com.zizaitianyuan.javac2.lesson02;
 
 import java.util.Arrays;
 import java.util.Random;
@@ -21,7 +21,7 @@ public class Practice {
 		}
 		System.out.println(Arrays.toString(array));
 		System.out.println(getMax(array));
-	}
+}
 	
 	
 	

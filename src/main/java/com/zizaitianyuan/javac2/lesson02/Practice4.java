1f70e6c311587b190d08939a15bb1eb7154b6171
songtianjian <524000659@qq.com>
2016-11-16 09:06:47 +0800

    practice4

--- src/main/java/com/zizaitianyuan/javac2/lesson02/Practice4.java
+++ src/main/java/com/zizaitianyuan/javac2/lesson02/Practice4.java
@@ -1,12 +1,14 @@
 package com.zizaitianyuan.javac2.lesson02;
 
+import java.util.Arrays;
+
 public class Practice4 {
 
 	/**
 	 * 参数给定一个内容为 0~9的整型数组， 要求此方法返回从小到大排好序的数组
 	 * 
-	 * 例如 给定 sourceArray={5, 8, 6, 5, 0, 0, 9, 2, 1} 
-	 * 返回结果应该是 {0, 0, 1, 2, 5, 5, 6, 8, 9};
+	 * 例如 给定 sourceArray={5, 8, 6, 5, 0, 0, 9, 2, 1} 返回结果应该是 {0, 0, 1, 2, 5, 5,
+	 * 6, 8, 9};
 	 * 
 	 * @param sourceArray
 	 *            一个给定的整型数组， 数组内容都是 0~9 的正数
@@ -14,7 +16,8 @@ public class Practice4 {
 	 */
 	public static int[] sort(int[] sourceArray) {
 		int[] result = new int[sourceArray.length];
-
-		return result;
+		Arrays.sort(sourceArray);
+		
+		return sourceArray;
 	}
 }

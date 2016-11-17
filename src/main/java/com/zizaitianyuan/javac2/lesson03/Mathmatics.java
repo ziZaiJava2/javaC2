04107232611c7c48315e5ba7cc99f6178be5cfc0
Shifeng Huang <shifeng.huang@hpe.com>
2016-11-17 14:03:51 +0800

    说明ｏｖｅｒｌｏａｄｉｎｇ

---
+++ src/main/java/com/zizaitianyuan/javac2/lesson03/Mathmatics.java
@@ -0,0 +1,61 @@
+package com.zizaitianyuan.javac2.lesson03;
+
+public class Mathmatics {
+
+	
+	public static int add(int a, int b) {
+		return a + b;
+	}
+	
+	public static int add(int a, int b, int c) {
+		return a + b + c;
+	}
+	
+	public static double add(double a, double b) {
+		return a + b;
+	}
+	
+	/*
+	 *  Java中返回值不算做签名的一部分，所以单独改变返回值的话，不能算作overload.
+	 *  
+	 *  取消下面的注释，会发现eclipse会报错说重复定义方法了
+	 */
+//	public static int add(double a, double b) {
+//		return (int)(a + b);
+//	}
+	
+	
+	// 补充减法，命名为sub
+	
+	
+	
+	/*
+	 * 做完题再补充一个知识点，可变参数列表
+	 * 
+	 * 我们继续重载add方法，这次这个新的方法可以接受任意数量整数
+	 */
+	public static int add(int ...args) {
+	//                        ^ 这里的...表达式的是可以接受任意个整数，而args的类型是一个***整数数组***
+		int sum = 0;
+		// 这里我们就可以用for-each来循环了，因为args是一个数组
+		for(int arg: args) {
+			sum += arg;
+		}
+		return sum;
+	}
+	
+	/*
+	 * 继续重载add, 可变参数可以和其他参数一起使用，但是可变参数列表必须放在最后一个.想想为什么？
+	 */
+	public static double add(double a, double ...args) {
+		double sum = a;
+		for(double arg : args) {
+			sum += arg;
+		}
+		return sum;
+	}
+	
+	/*
+	 * 自己尝试给sub添加可变参数列表
+	 */
+}

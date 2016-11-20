c1afcf91695b0628fc6fd6ae2af30c1665570ca3
xujialiang <928440876@qq.com>
2016-11-20 12:10:36 +0800

    11.20

--- src/main/java/com/zizaitianyuan/javac2/lesson06/ListMapPractice.java
+++ src/main/java/com/zizaitianyuan/javac2/lesson06/ListMapPractice.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /*
  *  练习要求， 按要求完善程序中的四个未完成的方法， 并且能够保证 ListMapPracticeTest 里的UT case都能够正确跑过。
@@ -16,60 +17,86 @@ import java.util.Map;
 
 public class ListMapPractice {
 	/*
-	 *  初始化一个用来存储所有学生的List， 最后面的代码块用来构造一些学生对象 并把他们保存在 allStudents中
+	 * 初始化一个用来存储所有学生的List， 最后面的代码块用来构造一些学生对象 并把他们保存在 allStudents中
 	 */
 	public static ArrayList<Student> allStudents = new ArrayList<Student>();
-	
+
 	// 一个Map 用来存储学生信息， key是学生的名字， value是学生
 	public static Map<String, Student> studentMapWithNameKey = new HashMap<String, Student>();
-	
+
 	// 一个Map 用来存储学生信息， key是学生的学号， value是学生
 	public static Map<Integer, Student> studentMapWithIdKey = new HashMap<Integer, Student>();
-	
+
 	/*
-	 *  从 allStudents 中找出主修专业为 "Software Engineering"的学生， 并保存在一个ArrayList中，并返回
+	 * 从 allStudents 中找出主修专业为 "Software Engineering"的学生， 并保存在一个ArrayList中，并返回
 	 */
-	public static List<Student> getStudentListMajoredInSoftwareEngineering(){
-		
+	public static List<Student> getStudentListMajoredInSoftwareEngineering() {
+
+		ArrayList<Student> ArrayList = new ArrayList<Student>();
+
+		for (Student major : allStudents) {
+			if ("Software Engineering".equals(major.getMajor())) {
+				ArrayList.add(major);
+			}
+		}
+		return ArrayList;
 	}
-	
-	
+
 	/*
-	 *  从 allStudents 中找出主修专业为 "Computer Science"的学生， 并保存在一个ArrayList中，并返回
+	 * 从 allStudents 中找出主修专业为 "Computer Science"的学生， 并保存在一个ArrayList中，并返回
 	 */
-	public static List<Student> getStudentListMajoredInComputerScience(){
+	public static List<Student> getStudentListMajoredInComputerScience() {
 		
+		ArrayList<Student> ArrayList = new ArrayList<Student>();
+
+		for (Student major : allStudents) {
+			if ("Computer Science".equals(major.getMajor())) {
+				ArrayList.add(major);
+			}
+		}
+		return ArrayList;
 	}
-	
+
 	/*
-	 * 通过 allStudents中保存的学生信息来构造一个 Map， 其中 key是学生的名字， value是学生, 以便以后可以快速的通过学生名字来查询学生的信息
+	 * 通过 allStudents中保存的学生信息来构造一个 Map， 其中 key是学生的名字， value是学生,
+	 * 以便以后可以快速的通过学生名字来查询学生的信息
 	 */
-	public static Map<String, Student> constructStudentMapWithNameKey(){
+	public static Map<String, Student> constructStudentMapWithNameKey() {
+//		Student student = new Student();
+		for (Student name : allStudents){
 		
-		
+		studentMapWithNameKey.put(name.getName(), name);
+		}
+
 		return studentMapWithNameKey;
 	}
-	
+
 	/*
-	 * 通过 allStudents中保存的学生信息来构造一个 Map， 其中 key是学生的学号， value是学生, 以便以后可以快速的通过学生学号来查询学生的信息
+	 * 通过 allStudents中保存的学生信息来构造一个 Map， 其中 key是学生的学号， value是学生,
+	 * 以便以后可以快速的通过学生学号来查询学生的信息
 	 */
-	public static Map<Integer, Student> constructStudentMapWithIdKey(){
-		
-		
+	public static Map<Integer, Student> constructStudentMapWithIdKey() {
+
+		for (Student id : allStudents){
+			
+			studentMapWithIdKey.put(id.getId(), id);
+			}
+	
 		return studentMapWithIdKey;
 	}
-	
-	
-	
+
 	/*
 	 * 构造一些学生对象 并把他们保存在 allStudents中
 	 */
-	static{
-		int[] studentIds = {100, 101, 102, 103, 104, 105, 106, 107};
-		String[] studentNames = {"Wei Wei", "Huang Shi Feng", "Xiao Ming", "Xiao Gang", "Han Mei Mei", "Li Lei", "Liu De Hua", "Sun Yan Zi"};
-		String[] studentMajor = {"Software Engineering", "Software Engineering", "Software Engineering", "Computer Science", "Computer Science", "Computer Science", "Computer Science", "Software Engineering"};
-		
-		for(int i=0; i< studentNames.length; i++){
+	static {
+		int[] studentIds = { 100, 101, 102, 103, 104, 105, 106, 107 };
+		String[] studentNames = { "Wei Wei", "Huang Shi Feng", "Xiao Ming", "Xiao Gang", "Han Mei Mei", "Li Lei",
+				"Liu De Hua", "Sun Yan Zi" };
+		String[] studentMajor = { "Software Engineering", "Software Engineering", "Software Engineering",
+				"Computer Science", "Computer Science", "Computer Science", "Computer Science",
+				"Software Engineering" };
+
+		for (int i = 0; i < studentNames.length; i++) {
 			Student student = new Student();
 			student.setId(studentIds[i]);
 			student.setName(studentNames[i]);
@@ -79,29 +106,34 @@ public class ListMapPractice {
 	}
 }
 
-class Student{
-	//学号
+class Student {
+	// 学号
 	private int id;
 	// 姓名
 	private String name;
-	//专业
+	// 专业
 	private String major;
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getMajor() {
 		return major;
 	}
+
 	public void setMajor(String major) {
 		this.major = major;
 	}
--- src/test/java/com/zizaitianyuan/javac2/lesson02/Practice2test.java
+++ src/test/java/com/zizaitianyuan/javac2/lesson02/Practice2test.java
@@ -8,19 +8,16 @@ public class Practice2test {
 	@Test
 	public void testcase2() {
 	
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
 		Assert.assertEquals(Practice2.fibonacci(134),8022285054220315881l);
-=======
+
 		Assert.assertEquals(Practice2.fibonacci3(32),2178309 );
->>>>>>> 010ecb7... abc
-=======
+
 		Assert.assertEquals(Practice2.fibonacci3(32),2178309 );
->>>>>>> 006926b... abc
-=======
+
 		Assert.assertEquals(Practice2.fibonacci(134),8022285054220315881l);
->>>>>>> c536710... Mathmatics.java
+
 	}
 
 }

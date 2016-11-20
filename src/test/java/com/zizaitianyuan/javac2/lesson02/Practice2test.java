8cca6072f08efd920213977c62e4e575c7029166
xujialiang <928440876@qq.com>
2016-11-20 09:28:02 +0800

    gg

--- src/test/java/com/zizaitianyuan/javac2/lesson02/Practice2test.java
+++ src/test/java/com/zizaitianyuan/javac2/lesson02/Practice2test.java
@@ -8,6 +8,7 @@ public class Practice2test {
 	@Test
 	public void testcase2() {
 	
+<<<<<<< HEAD
 
 
 		Assert.assertEquals(Practice2.fibonacci(134),8022285054220315881l);
@@ -18,6 +19,21 @@ public class Practice2test {
 
 		Assert.assertEquals(Practice2.fibonacci(134),8022285054220315881l);
 
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+		Assert.assertEquals(Practice2.fibonacci(134),8022285054220315881l);
+=======
+		Assert.assertEquals(Practice2.fibonacci3(32),2178309 );
+>>>>>>> 010ecb7... abc
+=======
+		Assert.assertEquals(Practice2.fibonacci3(32),2178309 );
+>>>>>>> 006926b... abc
+=======
+		Assert.assertEquals(Practice2.fibonacci(134),8022285054220315881l);
+>>>>>>> c536710... Mathmatics.java
+>>>>>>> d855e4a... gg
 	}
 
 }

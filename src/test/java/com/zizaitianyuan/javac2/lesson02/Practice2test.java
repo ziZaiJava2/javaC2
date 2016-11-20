d855e4aa8d62f6305a68e566c61b86d5f5356bd6
xujialiang <928440876@qq.com>
2016-11-20 09:28:02 +0800

    gg

---
+++ src/test/java/com/zizaitianyuan/javac2/lesson02/Practice2test.java
@@ -0,0 +1,26 @@
+package com.zizaitianyuan.javac2.lesson02;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Practice2test {
+	
+	@Test
+	public void testcase2() {
+	
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+		Assert.assertEquals(Practice2.fibonacci(134),8022285054220315881l);
+=======
+		Assert.assertEquals(Practice2.fibonacci3(32),2178309 );
+>>>>>>> 010ecb7... abc
+=======
+		Assert.assertEquals(Practice2.fibonacci3(32),2178309 );
+>>>>>>> 006926b... abc
+=======
+		Assert.assertEquals(Practice2.fibonacci(134),8022285054220315881l);
+>>>>>>> c536710... Mathmatics.java
+	}
+
+}

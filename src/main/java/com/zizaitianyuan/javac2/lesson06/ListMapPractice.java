441ab6ba4f4b1315825e2cfb095f1d12e293d97d
ZYYBOSS <1198688584@qq.com>
2016-11-20 12:13:23 +0800

    homework

--- src/main/java/com/zizaitianyuan/javac2/lesson06/ListMapPractice.java
+++ src/main/java/com/zizaitianyuan/javac2/lesson06/ListMapPractice.java
@@ -30,7 +30,15 @@ public class ListMapPractice {
 	 *  从 allStudents 中找出主修专业为 "Software Engineering"的学生， 并保存在一个ArrayList中，并返回
 	 */
 	public static List<Student> getStudentListMajoredInSoftwareEngineering(){
-		
+		ArrayList<Student> SoftEngineering = new ArrayList<Student>();
+		for(Student ttt :allStudents){
+			if("Software Engineering".equals(ttt.getMajor())){
+//				（我）格式：“字符串”.equals(遍历前面的名.get东西（）)
+				SoftEngineering.add(ttt);
+//				（我）因为调用的是ttt（allStudents已经没用了），所以直接写ttt
+			}
+		}
+		return SoftEngineering;
 	}
 	
 	
@@ -38,13 +46,27 @@ public class ListMapPractice {
 	 *  从 allStudents 中找出主修专业为 "Computer Science"的学生， 并保存在一个ArrayList中，并返回
 	 */
 	public static List<Student> getStudentListMajoredInComputerScience(){
-		
+		ArrayList<Student> ComputerScience = new ArrayList<Student>();
+		for(Student ttt :allStudents){
+			if("Computer Science".equals(ttt.getMajor())){
+				ComputerScience.add(ttt);
+			}
+		}
+		return ComputerScience;
 	}
 	
 	/*
 	 * 通过 allStudents中保存的学生信息来构造一个 Map， 其中 key是学生的名字， value是学生, 以便以后可以快速的通过学生名字来查询学生的信息
 	 */
 	public static Map<String, Student> constructStudentMapWithNameKey(){
+//		Map<String,Student> NameSearch = new HashMap<String,Student>（）；
+//		(我)父类前，子类后！但是前面已经定义过了！注意一下以后别犯傻。
+		for(Student ttt:allStudents){
+//		（我）ttt只是借用一下，应该是和后面的类型有父子关系，所以不用也不能用Map定义
+			studentMapWithNameKey.put(ttt.getName(), ttt);
+//		（我）赋值Map的格式：map名.put(key,value)注意别忘记分号
+		}
+		
 		
 		
 		return studentMapWithNameKey;
@@ -54,7 +76,9 @@ public class ListMapPractice {
 	 * 通过 allStudents中保存的学生信息来构造一个 Map， 其中 key是学生的学号， value是学生, 以便以后可以快速的通过学生学号来查询学生的信息
 	 */
 	public static Map<Integer, Student> constructStudentMapWithIdKey(){
-		
+		for(Student ttt:allStudents){
+			studentMapWithIdKey.put(ttt.getId(), ttt);
+		}
 		
 		return studentMapWithIdKey;
 	}

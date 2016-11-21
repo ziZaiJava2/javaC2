375c5288f6f860844bc18ba0d47f3eea19732023
xujialiang <928440876@qq.com>
2016-11-21 15:01:44 +0800

    re

--- src/main/java/com/zizaitianyuan/javac2/lesson03/Polymorphism.java
+++ src/main/java/com/zizaitianyuan/javac2/lesson03/Polymorphism.java
@@ -18,7 +18,13 @@ public class Polymorphism {
 		
 		// 我们的程序是生成一个对对应图形的说明，以及计算图形的周长 perimeter
 	for (String[] shape :data) {
-		Shape s =ShapeFactory.getInstance(shape[0], Integer.parseInt(shape[1]));
+		
+//		getInstance 在主函数开始时调用，返回一个实例化对象，此对象是static的，在内存中保留着它的引用，即内存中有一块区域专门用来存放静态方法和变量，
+//		* 可以直接使用，调用多次返回同一个对象。
+		
+//		Integer.parseInt(String)就是将String字符类型数据转换为Integer整型数据
+		
+		Shape s =ShapeFactory.getInstance(shape[0], Integer.parseInt(shape[1]));  
 		
 		System.out.print(s.toString()+ "，");
 		System.out.println("周长： "+s.perimeter());
@@ -53,6 +59,8 @@ class ShapeFactory{
 	}		
 }
 
+//implements是一个类实现一个接口用的关键字，它是用来实现接口中定义的抽象方法。
+//实现一个接口，必须实现接口中的所有方法。
 
 class ETriangle implements Shape {
 	
@@ -63,7 +71,13 @@ class ETriangle implements Shape {
 	}
 	@Override
 	public String toString(){
-		return String.format("等边三角形。 边长：%d",this.side);
+		
+//		String类新增了一个很有用的静态方法String.format(): 
+//			format(Locale l, String format, Object... args) 使用指定的语言环境、格式字符串和参数返回一个格式化字符串。 
+//			format(String format, Object... args) 使用指定的格式字符串和参数返回一个格式化字符串。
+//			举几个这个方法实用的例子(注释是输出结果)： 
+		
+		return String.format("等边三角形.边长：%d",this.side);
 	}
 	
 	@Override
@@ -82,7 +96,7 @@ class Circle implements Shape {
 	}
 	@Override
 	public String toString(){
-		return String.format("圆形。半径长：%d",this.r);
+		return String.format("圆形.半径长：%d",this.r);
 	}
 	
 	@Override
@@ -101,7 +115,7 @@ class Square implements Shape {
 	}
 	@Override
 	public String toString(){
-		return String.format("正方形。边长：%d",this.side);
+		return String.format("正方形.边长：%d",this.side);
 	}
 	
 	@Override

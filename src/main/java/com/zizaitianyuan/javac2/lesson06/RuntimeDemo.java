fdad96ffe76b0518183e6a58e5d973fcf422cf3a
Shifeng Huang <shifeng.huang@hpe.com>
2016-11-22 21:42:57 +0800

    runtime demo

---
+++ src/main/java/com/zizaitianyuan/javac2/lesson06/RuntimeDemo.java
@@ -0,0 +1,45 @@
+package com.zizaitianyuan.javac2.lesson06;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RuntimeDemo {
+
+	public static void main(String[] args) {
+		String name = new String("Kai");
+		
+		Foo aFoo = new Foo();
+		Foo foo = aFoo.append(name);
+		
+		System.out.println("size of aFoo: " + aFoo.strs.size());
+		System.out.println(aFoo == foo);
+		
+		
+		Foo anotherFoo = new Foo();
+		foo = anotherFoo.append(name);
+		System.out.println("size of anotherFoo: " + anotherFoo.strs.size());
+		System.out.println(anotherFoo == foo);
+		
+		System.out.println("size of Foo.allStrs: " + Foo.allStrs.size());
+	}
+	
+	
+	
+}
+
+class Foo {
+	
+	public static List<String> allStrs = new ArrayList<>();
+	
+	public List<String> strs = new ArrayList<>();
+	
+	public Foo append(String s) {
+		
+		List<String> all = Foo.allStrs;
+		all.add(s);
+		
+		this.strs.add(s);
+		
+		return this;
+	}
+}

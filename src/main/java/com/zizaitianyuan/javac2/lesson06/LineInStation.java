fef7d466743a599838b18129a3bf4c07b675502f
jamgao <1742036996@qq.com>
2016-11-22 15:51:49 +0800

    P

--- src/main/java/com/zizaitianyuan/javac2/lesson06/LineInStation.java
+++ src/main/java/com/zizaitianyuan/javac2/lesson06/LineInStation.java
@@ -3,6 +3,7 @@ package com.zizaitianyuan.javac2.lesson06;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 /*
  * 这个类用来模拟乘客进站排队买票。
@@ -21,39 +22,33 @@ import java.util.List;
  */
 public class LineInStation {
 
-	
 	private static List<Person> line1 = new ArrayList<>();
 	private static List<Person> line2 = new ArrayList<>();
 	private static List<Person> line3 = new ArrayList<>();
 	private static List<Person> line4 = new ArrayList<>();
-	
-	
+
 	public static void main(String[] args) {
-		
-		
-		
-		for(int i = 0; i < 10; i++) {
-			
+
+		for (int i = 0; i < 10; i++) {
+
 			saleTickets();
-			
+
 			List<Person> passages = comingPassagers();
 			lineUp(passages);
-			
+
 			passages = comingPassagers();
 			lineUp(passages);
-			
+
 			System.out.println(linesDesc());
 		}
-		
+
 		int passagers = passagersInLines();
-		
-		System.out.printf("总共有%d个乘客进入车站，卖出%d张车票，还有%d个乘客没有买到车票。\n", 
-				totalPassagers, saledTikets, passagers);
+
+		System.out.printf("总共有%d个乘客进入车站，卖出%d张车票，还有%d个乘客没有买到车票。\n", totalPassagers, saledTikets, passagers);
 	}
-	
-	
+
 	/*
-	 * 此方法应该随机生成一个长度为1~3的list，表示1~3个乘客来到车站， 
+	 * 此方法应该随机生成一个长度为1~3的list，表示1~3个乘客来到车站，
 	 * 
 	 * 并统计总的进站人数到(totalPassagers)
 	 * 
@@ -61,27 +56,50 @@ public class LineInStation {
 	 * 
 	 */
 	public static List<Person> comingPassagers() {
-		return null;
+
+		Random random = new Random();
+
+		int number = new Random().nextInt(3) + 1;
+
+		List<Person> passager = new ArrayList<Person>();
+
+		for (int i = 0; i < random.nextInt(3); i++) {
+
+			Person passagers = new Person(names.get(random.nextInt(names.size())));
+
+			passager.add(passagers);
+		}
+		totalPassagers += passager.size();
+
+		return passager;
 	}
-	
+
 	private static int totalPassagers = 0;
-	
-	private List<String> names = Arrays.asList(
-			"Jim", "Ken", "Leon", "Ada", "Lily", "Lucy", "Will", "Eagle",
-			"David", "Zoe", "Wendy", "Acea", "Kin", "Fish", "Paul", "Dean"
-			);
-	
-	
+
+	private static List<String> names = Arrays.asList("Jim", "Ken", "Leon", "Ada", "Lily", "Lucy", "Will", "Eagle",
+			"David", "Zoe", "Wendy", "Acea", "Kin", "Fish", "Paul", "Dean");
+
 	/*
-	 * 此方法将乘客排进队伍中，一同到来的乘客为了买票买到一起，他们会排到
-	 * 同一个队伍。
+	 * 此方法将乘客排进队伍中，一同到来的乘客为了买票买到一起，他们会排到 同一个队伍。
 	 * 
 	 * 乘客会挑选最短的队伍进行排队
 	 */
 	public static void lineUp(List<Person> passages) {
-		
+		List<Person> min = line1;
+
+		if (min.size() > line2.size()) {
+			min = line2;
+		}
+		if (min.size() > line3.size()) {
+			min = line3;
+		}
+		if (min.size() > line4.size()) {
+			min = line4;
+		}
+
+		min.addAll(passages);
 	}
-	
+
 	/*
 	 * 这个方法给每个队伍卖出一张车票，并统计总共卖出的票数到（saledTikets）。
 	 * 
@@ -90,38 +108,61 @@ public class LineInStation {
 	 * 就是排队伍第一个位置的乘客从队列中删除
 	 */
 	public static void saleTickets() {
-		
+		saleTiket(line1);
+
+		saleTiket(line1);
+
+		saleTiket(line1);
+
+		saleTiket(line1);
+
 	}
-	
+
+	private static void saleTiket(List<Person> line) {
+
+		if (!line.isEmpty()) {
+
+			saledTikets += 1;
+
+			line.remove(0);
+
+		}
+
+	}
+
+
+
 	/*
 	 * 统计还有多少个乘客没有买到车票
 	 */
 	public static int passagersInLines() {
-		return 0;
+		int passages = totalPassagers - saledTikets;
+		return passages;
 	}
-	
+
 	private static int saledTikets = 0;
-	
+
 	public static String linesDesc() {
-		return  "line1:" + line1.toString() + "\n" + 
-				"line2:" + line2.toString() + "\n" + 
-				"line3:" + line3.toString() + "\n" + 
-				"line4:" + line4.toString() + "\n";
+		return "line1:" + line1.toString() + "\n" + "line2:" + line2.toString() + "\n" + "line3:" + line3.toString()
+				+ "\n" + "line4:" + line4.toString() + "\n";
 	}
 }
 
-
 class Person {
 	private String name;
-	
+
 	public Person(String name) {
 		this.name = name;
 	}
-	
+
+	public void add(Person passagers) {
+
+	}
+
 	public String getName() {
 		return this.name;
 	}
-	
+
 	public String toString() {
 		return name;
 	}
---
+++ src/main/java/com/zizaitianyuan/javac2/washdish/Dishes.java
@@ -0,0 +1,13 @@
+package com.zizaitianyuan.javac2.washdish;
+
+public class Dishes {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
+public class Bowl implements getType{
+	
+}

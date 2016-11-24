aec1c44895d3f65ec2b1aa2bc0a6e4566ff15ea1
Shifeng Huang <shifeng.huang@hpe.com>
2016-11-24 22:16:46 +0800

    fix problem

--- src/test/java/com/zizaitianyuan/javac2/lesson06/practice/CoursTest.java
+++ src/test/java/com/zizaitianyuan/javac2/lesson06/practice/CoursTest.java
@@ -15,8 +15,8 @@ public class CoursTest {
 	@Test
 	public void test() {
 		CourseSession course = new CourseSession(01, "English");
-	    Assert.assertEquals(01, course.getCourseNum());
-	    Assert.assertEquals("English", course.getCourseName());
+	    assertEquals(01, course.getCourseNum());
+	    assertEquals("English", course.getCourseName());
 	}
 	@Test
 	public void test2(){
@@ -27,7 +27,7 @@ public class CoursTest {
 		course1.enroll(stu1);
 		course1.enroll(stu2);
         course1.enroll(stu3);
-        Assert.assertEquals(stu3, course1.indexStu(2));
+        assertEquals(stu3, course1.indexStu(2));
 	}
 	@Test
 	public void test3(){
@@ -36,21 +36,21 @@ public class CoursTest {
 		sc.set(2016, 11, 1);
 		date = sc.getTime();
 		CourseSession course = new CourseSession(01, "English", date);
-		Assert.assertEquals(date, course.getStartDate());
+		assertEquals(date, course.getStartDate());
 		Calendar ec = Calendar.getInstance();
 		ec.set(2017, 5, 1);
 		Date date1 = new Date();
 		date1 = ec.getTime();
-		Assert.assertEquals(date1, course.getEndDate());
+		assertEquals(date1, course.getEndDate());
 		
 	}
-	@SuppressWarnings("deprecation")
 	@Test
+	@SuppressWarnings("unused")
 	public void test4(){
 		CourseSession course1 = new CourseSession(01, "English");
 		CourseSession course2 = new CourseSession(02, "Math");
 		CourseSession course3 = new CourseSession(03, "Java");
-		Assert.assertEquals(3, course3.getCourseNum());
+		assertEquals(3, course3.getCourseNum());
 	}
 
 }

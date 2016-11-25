f6a813065a2d8835329f119b2fe7cdd4f9b1bfdf
songtianjian <524000659@qq.com>
2016-11-25 16:31:27 +0800

    000

--- src/main/java/com/zizaitianyuan/javac2/lesson06/bank/Account.java
+++ src/main/java/com/zizaitianyuan/javac2/lesson06/bank/Account.java
@@ -3,5 +3,15 @@ package com.zizaitianyuan.javac2.lesson06.bank;
 public class Account {
 	private String accountId;
 	private double moneyInAccount;
-	
+	public Account(String accountId, double moneyInAccount){
+		this.accountId = accountId;
+		this.moneyInAccount = moneyInAccount;
+	}
+	public String getAccuontIn(){
+		return accountId;
+	}
+	public double getMoneyInAccount(){
+		return moneyInAccount;
+		
+	}
 }

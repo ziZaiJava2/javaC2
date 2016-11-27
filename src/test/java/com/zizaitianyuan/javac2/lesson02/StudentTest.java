88ddfdd8dcafbed48381b65925fa4af76f7960e4
OhHelloWorld <18255629607@163.com>
2016-11-27 18:23:07 +0800

    jiarui

--- src/test/java/com/zizaitianyuan/javac2/lesson02/StudentTest.java
+++ src/test/java/com/zizaitianyuan/javac2/lesson02/StudentTest.java
@@ -8,41 +8,34 @@ import com.course.Student;
 
 public class StudentTest {
 	Student student = new Student("Jia");
-	Student student1=new Student("Rui");
-	Student student2=new Student("JiaRui");
+	Student student1 = new Student("Rui");
+	Student student2 = new Student("JiaRui");
+
 	@Test
-	public void testAddGrade1() {
+	public void testGetGPA1() {
+		double a = 8.0 / 3;
 		student.addGrade("A");
 		student.addGrade("B");
 		student.addGrade("D");
+		assertEquals(a, student.getGPA(), 0.01);
 	}
-	@Test
-	public void testGetGPA1() {
-		double a = 8.0/3;
-		assertEquals(a,student.getGPA(),0.01);
-	}
-	@Test
-	public void testAddGrade2() {
-		student1.addGrade("F");
-		student1.addGrade("F");
-		student1.addGrade("F");
-	}
+
 	@Test
 	public void testGetGPA2() {
 		double a = 0.0;
-		assertEquals(a,student1.getGPA(),0.01);
+		student1.addGrade("F");
+		student1.addGrade("F");
+		student1.addGrade("F");
+		assertEquals(a, student1.getGPA(), 0.01);
 	}
+
 	@Test
-	public void testAddGrade3() {
+	public void testGetGPA3() {
+		double a = 8.0 / 3;
 		student2.addGrade("a");
 		student2.addGrade("b");
 		student2.addGrade("d");
+		assertEquals(a, student2.getGPA(), 0.01);
 	}
-	@Test
-	public void testGetGPA3() {
-		double a = 8.0/3;
-		assertEquals(a,student2.getGPA(),0.01);
-	}
-	
 
 }

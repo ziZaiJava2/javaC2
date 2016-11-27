3f234bdbcacb3e9662ddb6fec496517c93ef62a3
OhHelloWorld <18255629607@163.com>
2016-11-27 13:21:56 +0800

    jiarui

--- src/main/java/com/course/DateUtil.java
+++ src/main/java/com/course/DateUtil.java
@@ -18,4 +18,24 @@ public class DateUtil {
 		Calendar cal = new GregorianCalendar(year, month, day);
 		return cal.getTime();
 	}
+
+	public static int dayCount(Date date) {
+		Calendar cal = new GregorianCalendar();
+		cal.setTime(date);
+		long time1 = cal.getTimeInMillis();
+		cal.setTime(new Date());
+		long time2 = cal.getTimeInMillis();
+		int between_days = (int) ((time1 - time2) / (1000 * 3600 * 24));
+		return between_days;
+	}
+
+	public static Date minDate(Date... arg) {
+		Date min = arg[0];
+		for (int i = 1; i < arg.length; i++) {
+			if (arg[i].before(min)) {
+				min = arg[i];
+			}
+		}
+		return min;
+	}
 }

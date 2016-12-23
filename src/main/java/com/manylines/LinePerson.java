eb63b796e14bdfc934131b5cbd6d7392cf567daf
ZYYBOSS <1198688584@qq.com>
2016-12-23 17:07:59 +0800

    thread not complete

--- src/main/java/com/manylines/LinePerson.java
+++ src/main/java/com/manylines/LinePerson.java
@@ -2,22 +2,21 @@ package com.manylines;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 //其实是排队买票的过程，类名字先不改
+
 public class LinePerson implements Runnable {
-	private String name;
-	private static int count = 6;
+	public static final Logger logger = LoggerFactory.getLogger(LinePerson.class);
+	private static int count = 15;
 	private List<Person> personList = new ArrayList<Person>();
 
-	public LinePerson(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
 	// 保证在票数减少时，大家是同步的
-	private synchronized void increase() {
+	private static synchronized void increase() {
 		count--;
 	}
 
@@ -27,17 +26,19 @@ public class LinePerson implements Runnable {
 	 */
 	@Override
 	public void run() {
-//		perList是自锁的
-		synchronized (personList) {
-		}
-		for (int i = 0; i < count; i++) {
+//第一次执行，当4个窗口都没 任务时候，即list为0，会同时进行count-1，打印出来是同一个数字，
+//没想到办法解决。
 			try {
-				if (personList.size() == 0) {
-					personList.add(new Person("ren"));
-					Thread.sleep(1000);
-					increase();
-					personList.remove(0);
-					System.out.println(name + "窗口执行了,还剩" + count + "张票");
+				while (true) {
+					if (personList.size() == 0) {
+						personList.add(new Person("ren"));// 由于本窗口，通过本列表有没有人，判断工作与否，所以PersonList操作要自锁
+						// }
+						increase();
+						// synchronized (this) {
+						personList.remove(0);
+						logger.info("count of ticket  {}", count);
+						Thread.sleep(1000);
+					}
 				}
 
 			} catch (InterruptedException e) {
@@ -45,19 +46,20 @@ public class LinePerson implements Runnable {
 			}
 		}
 
-	}
 
 	public static void main(String[] args) {
-		LinePerson m1 = new LinePerson("A");
-		LinePerson m2 = new LinePerson("B");
-		LinePerson m3 = new LinePerson("C");
 
-		Thread t1 = new Thread(m1);
-		Thread t2 = new Thread(m2);
-		Thread t3 = new Thread(m3);
-		t1.start();
-		t2.start();
-		t3.start();
+		// ExecutorService es = Executors.newCachedThreadPool();
+		ExecutorService es = Executors.newFixedThreadPool(4);
+
+		// es.execute(new LinePerson());
+
+		for (int i = 0; i < 4; i++) {
+			es.execute(new LinePerson());
+		}
+
+		es.shutdown();
+
 	}
 
 }
---
+++ src/main/java/com/threading/Person.java
@@ -0,0 +1,17 @@
+package com.threading;
+
+public class Person {
+	public Person(String name){
+		this.name = name;
+	}
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}

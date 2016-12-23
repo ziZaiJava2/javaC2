c91052993371a860da40ce102be90c5bf30941d6
OhHelloWorld <18255629607@163.com>
2016-12-23 22:20:14 +0800

    update thread

--- src/main/java/com/xiwangong/WashingDishes.java
+++ src/main/java/com/xiwangong/WashingDishes.java
@@ -5,51 +5,44 @@ import java.util.concurrent.Executors;
 
 public class WashingDishes {
 
+	protected static IntoConvey intoDeliver = new IntoConvey();
+
 	public static void main(String[] args) throws InterruptedException {
 
-		IntoConvey intoDeliver = new IntoConvey();
-				
 		ConveyorBelt wanDeliver = new ConveyorBelt();
-		
-		Cupboard wanGui = new Cupboard();
-		
-		XiWanGong1 first = new XiWanGong1(wanGui,wanDeliver);
-		
-		XiWanGong2 two = new XiWanGong2(wanGui,wanDeliver);
-		
-		ExecutorService es = Executors.newFixedThreadPool(2);
-				
-		for (int j = 0; j < 10; j++) {
 
-			intoDeliver.intoDeliver(intoDeliver.createBowl(), wanDeliver);
-				
-			es.execute(first);
-			
-			es.execute(two);
-			
-			Thread.sleep(0);
-						
-		}
-		
+		Cupboard wanGui = new Cupboard();
+
+		XiWanGong1 first = new XiWanGong1(wanGui, wanDeliver);
+
+		XiWanGong2 two = new XiWanGong2(wanGui, wanDeliver);
+
+		ExecutorService es = Executors.newFixedThreadPool(2);
+
+		es.execute(first);
+
+		es.execute(two);
+
 		es.shutdown();
-		
+
 		System.out.println("总共放入" + IntoConvey.IntoWanCount + "个碗");
-		
-		System.out.println("洗碗工"+first.getName()+"洗了"+first.getCount()+"个碗,"+"洗碗工"+two.getName()+"洗了"+two.getCount()+"个碗");
-		
+
+		System.out.println("洗碗工" + first.getName() + "洗了" + first.getCount() + "个碗," + "洗碗工" + two.getName() + "洗了"
+				+ two.getCount() + "个碗");
+
 		System.out.println("传送台还剩下" + ConveyorBelt.deliverWanCount + "个碗");
-		
+
 		System.out.println("碗柜里有" + Cupboard.wanGuiAcount + "个碗");
-		
+
 		if (IntoConvey.IntoWanCount == ConveyorBelt.deliverWanCount + Cupboard.wanGuiAcount) {
-		
+
 			System.out.println("jiarui1和jiarui2很厉害，没有丢一个碗");
-		
+
 		} else {
-			
-			System.out.println(
-					"丢了" + (IntoConvey.IntoWanCount - ConveyorBelt.deliverWanCount - Cupboard.wanGuiAcount) + "个碗，扣工资！！！");
-		
+
+			System.out.println("丢了" + (IntoConvey.IntoWanCount - ConveyorBelt.deliverWanCount - Cupboard.wanGuiAcount)
+					+ "个碗，扣工资！！！");
+
 		}
 
 	}
--- src/main/java/com/xiwangong/XiWanGong.java
+++ src/main/java/com/xiwangong/XiWanGong.java
@@ -2,57 +2,65 @@ package com.xiwangong;
 
 import java.util.Random;
 
-public class XiWanGong  implements Runnable{
-	
+public class XiWanGong implements Runnable {
+
 	public int count = 0;
-	
+
 	private int Acount = new Random().nextInt(3) + 3;
-	
+
 	private Cupboard wanGui;
-	
+
 	private ConveyorBelt wanDeliver;
-	
-	public XiWanGong(){}
-	
-	public XiWanGong(Cupboard wanGui,ConveyorBelt wanDeliver){
-		
-		this.wanGui = wanGui;
-		
-		this.wanDeliver = wanDeliver;
-		
+
+	private IntoConvey intoDeliver = WashingDishes.intoDeliver;
+
+	public XiWanGong() {
 	}
-	
+
+	public XiWanGong(Cupboard wanGui, ConveyorBelt wanDeliver) {
+
+		this.wanGui = wanGui;
+
+		this.wanDeliver = wanDeliver;
+
+	}
+
 	@Override
 	public void run() {
-		
-		wash();
-		
-	}
-	
-	private synchronized void wash(){
-		
-		for (int i = 0; i < Acount; i++) {
+				
+		while (true) {
 			
-			Bowl w = wanDeliver.removeBowl();
-			
-			System.out.println("洗刷刷");
-			
-			wanGui.CupboardAddBowl(w);
-			
-			count++;
-			
+			wash();
+
 		}
-		
 	}
-	
+
+	private synchronized void wash() {
+
+		intoDeliver.intoDeliver(intoDeliver.createBowl(), wanDeliver);
+
+		for (int i = 0; i < Acount; i++) {
+
+			Bowl w = wanDeliver.removeBowl();
+
+			// System.out.println("洗刷刷");
+
+			wanGui.CupboardAddBowl(w);
+
+			count++;
+
+		}
+
+	}
+
 	public int getCount() {
-		
+
 		return count;
 	}
-	
-	public String getName(){
-		
+
+	public String getName() {
+
 		return null;
-		
+
 	}
 }

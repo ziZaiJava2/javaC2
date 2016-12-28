91f49a845941ce4c0785bb2da31931f654964830
OhHelloWorld <18255629607@163.com>
2016-12-28 08:50:01 +0800

    update thread but error

--- src/main/java/com/station_thread/AddPassage.java
+++ src/main/java/com/station_thread/AddPassage.java
@@ -5,63 +5,65 @@ import java.util.Random;
 
 /**
  * 负责安排人进站的线程
+ * 
  * @author Administrator
  *
  */
-public class AddPassage implements Runnable{
-	
+public class AddPassage implements Runnable {
+
 	private List<List<String>> lines;
-	
-	private String[] passage = new String[]{"A","B","C","D"};
-	
+
+	private String[] passage = new String[] { "A", "B", "C", "D" };
+
 	private Random ran = new Random();
-	
+
 	private List<String> target = null;
-	
+
 	private boolean isFull = false;
-	
-	public static int passCount = 20;//在主函数里随机添加了20个起始的旅客，故passCount为20。
-	
+
+	public static int passCount = 20;// 在主函数里随机添加了20个起始的旅客，故passCount为20。
+
 	public AddPassage(List<List<String>> lines) {
-		
+
 		this.lines = lines;
-		
-	} 
-	
+
+	}
+
 	@Override
 	public void run() {
-		
-		while(!isFull){
-			
+
+		while (!isFull) {
+
 			try {
-			
+
 				add();
-			
+
 			} catch (InterruptedException e) {
 
 				e.printStackTrace();
-			
+
 			}
 
 		}
-		
+
 	}
+
 	/**
-	 * 随机添加人进入队伍，每个人进站间隔200ms，当人数为200时结束进站
-	 * 旅客会选择最短队伍排队
+	 * 随机添加人进入队伍，每个人进站间隔200ms，当人数为200时结束进站 旅客会选择最短队伍排队
+	 * 
 	 * @throws InterruptedException
 	 */
-	private void add() throws InterruptedException{
-		
+	private void add() throws InterruptedException {
+
+		int a = ran.nextInt(4);
+
 		synchronized (AddPassage.class) {
 			
-			int a = ran.nextInt(4);
-			
 			target = lines.get(0);
-			
+
 			for (List<String> line : lines) {
 				
-				if(line.size() < target.size()){
+				if (line.size() < target.size()) {
 					
 					target = line;
 					
@@ -69,21 +71,27 @@ public class AddPassage implements Runnable{
 				
 			}
 			
+		}
+		
+		synchronized (target) {
+
+			
 			target.add(passage[a]);
-			
+
 			passCount++;
-			
-			System.out.println("现在车站有"+passCount+"个人");
-			
-			if(passCount > 200){
+
+			System.out.println("现在车站有" + passCount + "个人");
+		
+			if (passCount > 200) {
 				
 				isFull = true;
 				
 			}
-			
-			Thread.sleep(100);
-			
-		}		
+
+		}
+
+		Thread.sleep(100);
+
 	}
 
 }
--- src/main/java/com/station_thread/StationWindow.java
+++ src/main/java/com/station_thread/StationWindow.java
@@ -4,17 +4,18 @@ import java.util.List;
 
 /**
  * 负责窗口售票的线程
+ * 
  * @author Administrator
  *
  */
 public class StationWindow implements Runnable {
 
 	private List<String> line;
-	
+
 	private boolean isGG = false;
-	
-	private int ticket = 200;//每个窗口都有200张票
-	
+
+	private int ticket = 200;// 每个窗口都有200张票
+
 	private String name;
 
 	public StationWindow(List<String> line, String name) {
@@ -29,13 +30,13 @@ public class StationWindow implements Runnable {
 
 		while (!isGG) {
 			try {
-			
+
 				Thread.sleep(100);
-			
+
 			} catch (InterruptedException e) {
 
 				e.printStackTrace();
-			
+
 			}
 			saledTickets();
 
@@ -44,33 +45,35 @@ public class StationWindow implements Runnable {
 	}
 
 	/**
-	 * 因为四个窗口和队伍都互相独立，所以只需锁对象本身即可。
+	 * 因为窗口与排队处理的是同一个list，需锁list。
 	 */
-	private synchronized void saledTickets() {
+	private void saledTickets() {
 
-		if (line.size() > 0) {
-	
-			System.out.println("现在车站有" + (--AddPassage.passCount) + "人");
-
-			String pass = line.remove(0);
+		synchronized (line) {
 			
-			System.out.println(pass+"买到了回家的票");
+			if (line.size() > 0) {
 
-			ticket--;
+				System.out.println("现在车站有" + (--AddPassage.passCount) + "人");
 
+				String pass = line.remove(0);
 
-			if (ticket <= 0) {
+				System.out.println(pass + "买到了回家的票");
 
-				System.out.println("***"+name + "票已经卖光了***");
-				
-				isGG = true;
+				ticket--;
 
-			}else if(ticket % 10 ==0){
-				
-				System.out.println(name+"***还剩"+ticket+"张票***");
-				
+				if (ticket <= 0) {
+
+					System.out.println("***" + name + "票已经卖光了***");
+
+					isGG = true;
+
+				} else if (ticket % 10 == 0) {
+
+					System.out.println(name + "***还剩" + ticket + "张票***");
+
+				}
+
 			}
-
 		}
 	}
 

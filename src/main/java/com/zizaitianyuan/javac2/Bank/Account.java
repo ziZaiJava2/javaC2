8b75e93c9b7679bffc001412dd9ec3f5d5cf768b
Spider <523995513@qq.com>
2017-06-30 18:58:49 +0800

    银行练习 Account.java

---
+++ src/main/java/com/zizaitianyuan/javac2/Bank/Account.java
@@ -0,0 +1,30 @@
+package com.zizaitianyuan.javac2.Bank;
+
+/**
+ * Created by SPIDER on 2017/6/30.
+ */
+public class Account {
+    private String accountId;
+    private double moneyInAccount;
+
+    Account(String accountId,double moneyInAccount){
+        this.accountId=accountId;
+        this.moneyInAccount=moneyInAccount;
+    }
+
+    public String getAccountId(){
+        return accountId;
+    }
+
+    public double getMoneyInAccount(){
+        return moneyInAccount;
+    }
+
+    public void setAccountId(String accountId){
+        this.accountId=accountId;
+    }
+
+    public void setMoneyInAccount(double moneyInAccount){
+        this.moneyInAccount=moneyInAccount;
+    }
+}

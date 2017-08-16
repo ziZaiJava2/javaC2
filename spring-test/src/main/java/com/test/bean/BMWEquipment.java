09ba8a109c9549eda0737e4f6c4ea4587bf357a9
Robert <925016145@qq.com>
2017-08-16 16:33:32 +0800

    spring

--- spring-test/src/main/java/com/test/bean/BMWEquipment.java
+++ spring-test/src/main/java/com/test/bean/BMWEquipment.java
@@ -1,8 +1,11 @@
 package com.test.bean;
 
 public class BMWEquipment implements Equipment{
-	
+	CarDAO carDAO;
 	public void produceCar(){
 		System.out.println("produce BMW");
+	}
+	public void setCarDAO(CarDAO carDao){
+		this.carDAO = carDao;
 	}
 }
--- spring-test/src/main/java/com/test/main/CarMain.java
+++ spring-test/src/main/java/com/test/main/CarMain.java
@@ -3,6 +3,7 @@ package com.test.main;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.test.bean.BMWEquipment;
 import com.test.bean.CarDAO;
 import com.test.bean.DasAutoEquipment;
 
@@ -14,8 +15,13 @@ public class CarMain {
 		//容器的配置
 		ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
 		
+		//大众的配置
 		DasAutoEquipment dasAutoEquipment = (DasAutoEquipment)context.getBean("dasAutoEquipment");
 		
+		//宝马的配置	
+		BMWEquipment bmwEquipment = (BMWEquipment)context.getBean("bmwEquipment");
+		
 		dasAutoEquipment.produceCar();
+		bmwEquipment.produceCar();
 	}
 }
